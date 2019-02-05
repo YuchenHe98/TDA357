744d632b2b58c65e35eef7874ae8c1357290c01d
Yuchen He <yuchenhe@Yuchens-MacBook-Pro.local>
2019-02-05 13:30:40 +0100

    remove comment

--- view.sql
+++ view.sql
@@ -18,16 +18,6 @@ CREATE VIEW Registrations(student, course, status) AS
 SELECT student, course, 'registered' AS status FROM Registered
 UNION
 SELECT student, course, 'waiting' AS status FROM WaitingList
-    
-    
-    
-    
-    
-    SELECT idnr, Students.program, StudentBranches.branch, Courses.code
-    FROM Students, MandatoryBranch, MandatoryProgram, Courses
-    WHERE (idnr, Students.program) NOT IN (
-		SELECT student, program FROM StudentBranches
-	) GROUP BY idnr
 	
 /*
 View: BasicInformation(idnr, name, login, program, branch) For all students, their national identification number, name, login, their program and the branch (if any). The branch column is the only column in any of the views that is allowed to contain NULL.
